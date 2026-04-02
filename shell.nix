2c06ca35401a00d04cef7fe74da3a76577705d92
Long Vu <hlongvu@gmail.com>
2026-04-02 19:14:16 +0700

    add nix shell

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs
+    pkgs.electron
+  ];
+  shellHook = ''
+    export ELECTRON_OVERRIDE_DIST_PATH="${pkgs.electron}/bin"
+  '';
+}
